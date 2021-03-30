07fadc443ddc4294ca8c1ed8eea88c2fd325b515
sotakon <skon1221@gmail.com>
2021-03-30 11:30:11 +0900

    メッセージ機能のバグ修正

--- app/controllers/admins_controller.rb
+++ app/controllers/admins_controller.rb
@@ -2,14 +2,14 @@ class AdminsController < ApplicationController
   before_action :if_not_admin
   before_action :set_restaurant, only: [:show, :edit, :destroy]
 
-def index
-  @users = User.all
-  @corporations = Corporation.all
-  @jobs = Job.all
-  @rumors =Rumor.all
-end
+  def index
+    @users = User.all
+    @corporations = Corporation.all
+    @jobs = Job.all
+    @rumors =Rumor.all
+  end
 
-  private
+private
   def if_not_admin
     if current_user != nil && current_user.admin?
     elsif current_corporation || current_user || current_user == nil
