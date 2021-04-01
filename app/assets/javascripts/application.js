b6195f1be993f6d7e374e05f2d5dba2a2ec56099
sotakon <skon1221@gmail.com>
2021-04-01 15:08:10 +0900

    フッター導入

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -25,15 +25,5 @@ $(window).scroll(function(){
   else {
     $('.header').removeClass('fixed-top');
   }
-  //Footer
-    var navpanel = $('.bottom-nav');
-    var panelHeight = navpanel.height() - 4;
-    var footerHeight = $('.footer').height();
-    if( $(document).height() - panelHeight - footerHeight <= $(window).scrollTop() + $(window).height() ) {
-      navpanel.removeClass('fixed-bottom');
-    } 
-  else {
-    navpanel.addClass('fixed-bottom');
-  }
   
 });
