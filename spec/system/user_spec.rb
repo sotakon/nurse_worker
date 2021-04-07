08df18a30c00d50a0524e9b3eb88f5fdf17935d1
sotakon <skon1221@gmail.com>
2021-04-07 21:29:33 +0900

    rspec導入

---
+++ spec/system/user_spec.rb
@@ -0,0 +1,21 @@
+require 'rails_helper'
+RSpec.describe 'ユーザー機能', type: :system do
+  describe '新規登録機能' do
+    context 'ユーザーが新規登録した場合' do
+      it 'マイページが表示される' do
+      end
+    end
+  end
+  describe '一覧表示機能' do
+    context '一覧画面に遷移した場合' do
+      it '作成済みのタスク一覧が表示される' do
+      end
+    end
+  end
+  describe '詳細表示機能' do
+     context '任意のタスク詳細画面に遷移した場合' do
+       it '該当タスクの内容が表示される' do
+       end
+     end
+  end
+end
