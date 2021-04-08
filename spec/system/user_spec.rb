f6b9440459d7a95557f1798a6a84c79103e1810e
sotakon <skon1221@gmail.com>
2021-04-08 14:54:58 +0900

    リストマーカー削除

--- spec/system/user_spec.rb
+++ spec/system/user_spec.rb
@@ -61,5 +61,16 @@ RSpec.describe 'ユーザー機能', type: :system do
         expect(page).to have_content 'テスト2'
       end
     end
+
+    context 'ユーザーがユクチコミを投稿した場合' do
+      it 'クチコミが表示される' do
+        visit new_rumor_path
+        fill_in 'rumor[name]', with: 'テスト'
+        fill_in 'rumor[area]', with: '20'
+        fill_in 'user[area]', with: 'テスト'
+        fill_in 'user[email]', with: 'test@gmail.com'
+
+      end
+    end
   end
 end
