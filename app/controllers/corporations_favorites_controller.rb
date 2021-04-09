c55372a6efe15ad5128991b4fcebef7240e59adb
sotakon <skon1221@gmail.com>
2021-04-09 15:56:52 +0900

    面接オファー後のpath修正

--- app/controllers/corporations_favorites_controller.rb
+++ app/controllers/corporations_favorites_controller.rb
@@ -3,11 +3,11 @@ class CorporationsFavoritesController < ApplicationController
     @favorite = current_corporation.corporations_favorites.create(user_id: params[:user_id])
     OfferMailer.offer_mail(@favorite).deliver
     OfferMailer.offer_corporation_mail(@favorite).deliver
-    redirect_to users_path, notice: "#{@favorite.user.name}さんにスカウトメールを送りました"
+    redirect_to user_path(@favorite.user.id), notice: "#{@favorite.user.name}さんにスカウトメールを送りました"
   end
 
   def destroy
     @favorite = current_corporation.corporations_favorites.find_by(id: params[:id]).destroy
-    redirect_to users_path, notice: "#{@favorite.user.name}さんをリストから削除しました"
+    redirect_to corporation_path(current_corporation), notice: "#{@favorite.user.name}さんをリストから削除しました"
   end
 end 
