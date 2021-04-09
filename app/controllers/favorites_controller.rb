e95f51328ed9ce81320ee530b5904c2c02efc19d
sotakon <skon1221@gmail.com>
2021-04-09 16:07:30 +0900

    エントリー後のpath修正

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -3,11 +3,11 @@ class FavoritesController < ApplicationController
     @favorite = current_user.favorites.create(job_id: params[:job_id])
     FavoriteMailer.favorite_mail(@favorite).deliver
     FavoriteMailer.favorite_corporation_mail(@favorite).deliver
-    redirect_to jobs_path, notice: "#{@favorite.job.corporation.name}の求人にエントリーしました"
+    redirect_to job_path(@favorite.job), notice: "#{@favorite.job.name}の求人にエントリーしました"
   end
 
   def destroy
     @favorite = current_user.favorites.find_by(id: params[:id]).destroy
-    redirect_to jobs_path, notice: "#{@favorite.job.corporation.name}の求人をリストから削除しました"
+    redirect_to user_path(current_user), notice: "#{@favorite.job.name}の求人をリストから削除しました"
   end
 end
