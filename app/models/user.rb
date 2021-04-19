e03049e6d4191abe850eb2cf8fb1cc5de6fda840
sotakon <skon1221@gmail.com>
2021-04-19 17:18:49 +0900

    update: 18際未満は登録できないよう修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,7 +9,7 @@ class User < ApplicationRecord
   has_many :following, through: :active_relationships, source: :followed
   has_many :followers, through: :passive_relationships, source: :follower
       validates :name, presence: true
-      validates :age, presence: true
+      validates :age, numericality: { only_integer: true, greater_than_or_equal_to: 18 }
       validates :area, presence: true
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,19 +3,25 @@ describe 'ユーザーモデル機能', type: :model do
   describe 'バリデーションのテスト' do
     context 'ユーザーの名前が空の場合' do
       it 'バリデーションにひっかる' do
-        user = User.new(name: '', age: '失敗テスト', area: '失敗テスト')
+        user = User.new(name: '', age: '失敗テスト', area: '失敗テスト',email: 'test@test.com', password: '123456')
         expect(user).not_to be_valid
       end
     end
     context 'ユーザーの年齢が空の場合' do
       it 'バリデーションにひっかる' do
-        user = User.new(name: '失敗テスト', age: '', area: '失敗テスト')
+        user = User.new(name: '失敗テスト', age: '', area: '失敗テスト',email: 'test@test.com', password: '123456')
+        expect(user).not_to be_valid
+      end
+    end
+    context 'ユーザーの年齢が18際未満の場合' do
+      it 'バリデーションにひっかる' do
+        user = User.new(name: '失敗テスト', age: '17', area: '失敗テスト',email: 'test@test.com', password: '123456')
         expect(user).not_to be_valid
       end
     end
     context 'ユーザーのエリアが空の場合' do
       it 'バリデーションにひっかる' do
-        user = User.new(name: '失敗テスト', age: '失敗テスト', area: '')
+        user = User.new(name: '失敗テスト', age: '失敗テスト', area: '',email: 'test@test.com', password: '123456')
         expect(user).not_to be_valid
       end
     end
