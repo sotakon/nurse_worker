7050c4290475eed1b5ac4122b3a3528a3d27a0ca
sotakon <skon1221@gmail.com>
2021-04-20 14:38:47 +0900

    test: production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -97,7 +97,7 @@ Rails.application.configure do
   ActionMailer::Base.smtp_settings = {
     user_name: ENV['SENDGRID_USERNAME'],
     password: ENV['SENDGRID_PASSWORD'],
-    domain: "	em3308.nurse.worker.com",
+    domain: "info@nurse.worker.com",
     address: "smtp.sendgrid.net",
     port: 587,
     authentication: :plain,
