6ea24a283781613f1119d91b0ff2848095848948
sotakon <skon1221@gmail.com>
2021-04-21 15:52:23 +0900

    update: エラー画面に画像実装

--- app/controllers/corporations_controller.rb
+++ app/controllers/corporations_controller.rb
@@ -5,15 +5,11 @@ class CorporationsController < ApplicationController
     @job = Job.all
     @corporation = Corporation.find(params[:id])
   
-
-
     if current_corporation
       favorites = CorporationsFavorite.where(corporation_id: current_corporation.id).pluck(:user_id)
       @favorite_corporation = User.find(favorites)
       offers = Favorite.where(job_id: @job.ids).pluck(:user_id)
       @favorite_users = User.find(offers)
-      # @current_job = current_corporation.jobs.find()
-      # binding.irb
     end
   end
 end
